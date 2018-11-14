33dc75e5fbb65cff7d270111d9136b94d3d4a815
asahi <asahi_hm@msn.com>
2018-11-14 12:13:55 +0800

    exercise of Function.

--- new1.py
+++ new1.py
@@ -12,18 +12,24 @@
 #         print(message)
 
 # Exercise 7-6
-prompt = 'Please tell me how old you are:'
-active = True
-while active:
-    year = input(prompt)
-    if year == 'quit' or year == 'exit':
-        active = False
-        continue
-    else:
-        year = int(year)
-    if year <= 3:
-        print('you got a free ticket.')
-    elif year <= 12:
-        print('The price is 10 bucks.')
-    elif year > 12:
-        print('The price is 15 bucks.')
+# prompt = 'Please tell me how old you are:'
+# active = True
+# while active:
+#     year = input(prompt)
+#     if year == 'quit' or year == 'exit':
+#         active = False
+#         continue
+#     else:
+#         year = int(year)
+#     if year <= 3:
+#         print('you got a free ticket.')
+#     elif year <= 12:
+#         print('The price is 10 bucks.')
+#     elif year > 12:
+#         print('The price is 15 bucks.')
+
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
