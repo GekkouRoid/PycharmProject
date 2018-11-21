0728c03c3c9786351d107778b195f7597afe4de4
asahi <asahi_hm@msn.com>
2018-11-21 22:18:32 +0800

    creation of class Privileges.

---
+++ Privileges.py
@@ -0,0 +1,11 @@
+# Exercise of 9-8
+
+
+class Privileges:
+    def __init__(self):
+        self.privileges = ['Add post', 'delete post']
+
+    def describe_privileges(self):
+        print(self.privileges)
+
+
