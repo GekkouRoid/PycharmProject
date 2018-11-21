cc4ef82df684bbab7dedeea37ac79080cb301867
asahi <asahi_hm@msn.com>
2018-11-21 22:12:37 +0800

    exercise of 9-7

---
+++ classtest4.py
@@ -0,0 +1,16 @@
+# Exercise of 9-7
+
+from classtest2 import User
+
+
+class Admin(User):
+    def __init__(self, firstname, lastname,sexuality='male', career='engineer'):
+        super().__init__(firstname, lastname, sexuality='male', career='engineer')
+        self.privileges = ['Can Add Post', 'Can Delete Post']
+
+    def describe_privileges(self):
+        print(self.privileges)
+
+
+admin1 = Admin('Mike', 'water')
+admin1.describe_privileges()
