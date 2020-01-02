20f25201c56d6d06b572c06daf26ca3f3ec0af2e
asahi <gekkou.hm@gmail.com>
2020-01-02 21:53:03 +0800

    new

--- test1.py
+++ test1.py
@@ -1,3 +1,10 @@
-import unittest
-import exceptiontest1
+# import unittest
+# import exceptiontest1
 
+while True:
+    try:
+        age = int(input("Age is :"))
+        print (f'Age is {age}')
+        break
+    except ValueError:
+        print ("Input is invalid.")
